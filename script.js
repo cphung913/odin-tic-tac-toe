2fbd25cdc04d58aab006f38b88b6a517a956224e
cphung913 <chasephung13@gmail.com>
2023-07-14 17:16:37 -0700

    setup board filling function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const gameBoard = (() => {
-    let a = "";
-    let b = "yay";
-    return { a, b }
+    let boardValues = ["", "", "", "", "", "", "", "", ""];
+    const board = document.querySelector(".board");
+    const tiles = Array.from(board.childNodes).filter(x => x.nodeName != "#text");
+    function setBoard() {
+        for (let i = 0; i < tiles.length; i++) {
+            tiles[i].textContent = boardValues[i];
+        }
+    }
+
+    return { setBoard }
 })()
+
+gameBoard.setBoard();
